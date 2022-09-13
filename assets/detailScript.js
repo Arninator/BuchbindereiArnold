37650500493a96b05916ebe55d4c2d1b12455b45
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-13 17:17:44 +0000

    add header tag insteead of div around menu

---
+++ assets/detailScript.js
@@ -0,0 +1,13 @@
+class DetailPage extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render () {
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
