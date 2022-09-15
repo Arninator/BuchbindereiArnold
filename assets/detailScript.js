cd0c4ca2593cb7c4eced319f8acb106abbef509b
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-15 09:50:58 +0000

    still try to load data from json file and assign it to an variable

--- assets/detailScript.js
+++ assets/detailScript.js
@@ -1,16 +1,37 @@
-import {Data} from "content.json"
+
 
 class DetailPage extends React.Component {
     constructor(props) {
         super(props);
-    }
 
+        this.state = {
+            data: ""
+        }
+    }
+    componentWillMount() {
+        var data;
+
+        fetch('../assets/content.json')
+        .then((response) => response.json())
+        .then((findresponse) => {
+            this.setState({
+                data: findresponse
+        })
+            data = findresponse;
+            // console.log(JSON.stringify(data));
+            console.log("WILL: " + JSON.stringify(this.state.data));
+            return findresponse;
+        })
+        
+        console.log(JSON.stringify(data));
+    }
+    componentDidMount() {
+        console.log("DID: " + JSON.stringify(this.state.data));
+    }
     render () {
-        console.log("blas")
-        console.log(JSON.stringify(Data));
         return(
             <section className="detail-section">
-                <div>BLaA</div>
+                <div>BLA</div>
             </section>
         )
     }
