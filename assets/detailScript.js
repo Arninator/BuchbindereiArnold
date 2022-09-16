fda71e3204eaf1cffdaf7e8c33e6740690e21904
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-16 17:32:12 +0000

    add style and configure the images and the figcaptions

--- assets/detailScript.js
+++ assets/detailScript.js
@@ -5,27 +5,30 @@ let currentSection;
 class DetailPage extends React.Component {
     constructor(props) {
         super(props);
+
+        this.handleClick = this.handleClick.bind(this);
     }
     componentWillMount() {
         data = JSON.parse(sessionStorage.getItem("data"));
         currentIndex = parseInt(sessionStorage.getItem("currentIndex"));
         currentSection = sessionStorage.getItem("currentSection");
-
-        console.log(currentIndex + currentSection);
     }
     componentDidMount() {
         $(".prev").prop('disabled', true);
+    }
+    handleClick() {
+
     }
     render () {
         return(
             <section className="detail-section">
                 <div className="shortcuts-div">
-                    {data["bucheinbaende"].map(obj => {
-                        return <Dot id={obj.name} className="shortcuts" />
+                    {data[currentSection].map(obj => {
+                        return <Shortcuts id={obj.name} className="shortcuts" onClick={this.handleClick}/>
                     })}
                 </div>
                 <div className="grossansicht-div">
-                    {data["bucheinbaende"].map(obj => {
+                    {data[currentSection].map(obj => {
                         return <Img id={obj.name} src={obj.urls} description={obj.description} title={obj.title} subtitle={obj.subtitle} foto={obj.foto}/>
                     })}
                 </div>
@@ -33,9 +36,9 @@ class DetailPage extends React.Component {
         )
     }
 }
-const Dot = (props) => {
+const Shortcuts = (props) => {
     return(
-        <div id={props.id} className={props.className}></div>
+        <div id={props.id} className={props.className} onClick={props.onClick}></div>
     )
 }
 const Img = (props) => {
@@ -44,23 +47,22 @@ const Img = (props) => {
     return(
         <figure className="figure">
             <div className="image-div">
-                <a className="prev" style={props.id != currentImg ? {display: 'none'} : {}}>&#10094;</a>
+                <button className="prev" style={props.id != currentImg ? {display: 'none'} : {}}>&#10094;</button>
                 {props.src.map((url, index) => {
-                    console.log(props.id);
-                    return <img id={props.id} src={url} alt={props.description[index]} style={props.id != currentImg ? {display: 'none'} : {}} />
+                    console.log(index);
+                    return <img id={props.id} src={url} alt={props.description[index]} style={props.id != currentImg || index != 0 ? {display: 'none'} : {}} />
                 })}
-                <a className="next" style={props.id != currentImg ? {display: 'none'} : {}}>&#10095;</a>
+                <button className="next" style={props.id != currentImg ? {display: 'none'} : {}}>&#10095;</button>
             </div>
             {props.description.map((description, index) => {
                 return(
-                    <figcaption style={props.id != currentImg ? {display: 'none'} : {}}>
-                        <h5>{props.title[index]}</h5>
-                        <h6>{props.subtitle[index]}</h6>
-                        <p>{description}<br></br><br></br><span>{props.foto[index]}</span></p>
+                    <figcaption style={props.id != currentImg || index != 0 ? {display: 'none'} : {}}>
+                        <h5 className="title" dangerouslySetInnerHTML={{__html:  props.title[index]}}></h5>
+                        <h6 className="subtitle" dangerouslySetInnerHTML={{__html:  props.subtitle[index]}}></h6>
+                        <p className="description" dangerouslySetInnerHTML={{__html:  description + "<br><br><span>" + props.foto[index] + "</span>"}}></p>
                     </figcaption>
                 )                    
             })}
-            
         </figure>
     )
 }
