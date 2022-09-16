a3b39fea5f73934b1f7bb7bf281d945f8068c00f
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-16 08:54:47 +0000

    build the detailPage with the big images

--- assets/detailScript.js
+++ assets/detailScript.js
@@ -7,12 +7,20 @@ class DetailPage extends React.Component {
     componentWillMount() {
         data = JSON.parse(sessionStorage.getItem("data"));
     }
+    componentDidMount() {
+        $(".prev").prop('disabled', true);
+    }
     render () {
         return(
             <section className="detail-section">
                 <div className="shortcuts-div">
                     {data["bucheinbaende"].map(obj => {
                         return <Dot id={obj.name} className="shortcuts" />
+                    })}
+                </div>
+                <div className="grossansicht-div">
+                    {data["bucheinbaende"].map(obj => {
+                        return <Img id={obj.name} src={obj.url} description={obj.description}/>
                     })}
                 </div>
             </section>
@@ -22,6 +30,20 @@ class DetailPage extends React.Component {
 const Dot = (props) => {
     return(
         <div id={props.id} className={props.className}></div>
+    )
+}
+const Img = (props) => {
+    return(
+        <figure className="figure">
+            <div className="image-div">
+                <a className="prev" >&#10094;</a>
+                <img id={props.id} src={props.src} alt={props.description} />
+                <a className="next" >&#10095;</a>
+            </div>
+            <figcaption>
+                {props.description}
+            </figcaption>
+        </figure>
     )
 }
 
