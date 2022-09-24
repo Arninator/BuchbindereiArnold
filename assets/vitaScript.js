407e33987da7df2dae02b5aff27b2a77dd8bbfd4
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-24 21:40:19 +0200

    add content of Vita

--- assets/vitaScript.js
+++ assets/vitaScript.js
@@ -4,7 +4,26 @@ class Vita extends React.Component {
     }
     render() {
         return(
-            <div></div>
+            <section>
+                <div class="vita-div">
+                    <h1>Vita</h1>
+                    <p>Mitglied des MdE (Meister der Einbandkunst, Internationale Vereinigung e.V.)</p>
+                    <p>seit 2013</p>
+                    <p>Selbständige Tätigkeit – Übernahme der Buchbinderei <span>Ingeborg M. Hartmann</span> in Hamburg</p>
+                    <p>2008 – 2013</p>
+                    <p>Mitarbeiterin in der Buchbinderei <span>Karen Begemann</span> in Hamburg</p>
+                    <p>2006 – 2008</p>
+                    <p>Ausbildung zur Buchbinderin in der Buchbinderei und Restaurierwerkstatt <span>Anke Metz</span> in Hamburg</p>
+                    <p>ab 2006</p>
+                    <p>Freie Mitarbeiterin in unterschiedlichen Landschaftsarchitekturbüros</p>
+                    <p>1990 – 2006</p>
+                    <p>Freie Landschaftsarchitektin und Tätigkeit in verschiedenen Planungsbüros</p>
+                    <p>1983 – 1990</p>
+                    <p>Studium der Landschaftsarchitektur an der TU Dresden</p>
+                    <p>Abschluss mit dem Diplom der Fachrichtung Landschaftsarchitektur</p>
+                    <p>geboren 1964</p>
+                </div>
+            </section>
         )
     }
 }
