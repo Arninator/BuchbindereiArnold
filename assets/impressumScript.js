38376536fcca3e47b779b546e87c9b4bc32d4f24
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-24 20:32:47 +0200

    build impressumScript

---
+++ assets/impressumScript.js
@@ -0,0 +1,11 @@
+class Impressum extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>BLALALA</div>
+        )
+    }
+}
+ReactDOM.render(<Impressum />, document.getElementById("impressum-page"))
