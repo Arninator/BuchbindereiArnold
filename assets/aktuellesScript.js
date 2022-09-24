67bc14e076f5e4c335127719f517cecbfeb9e08f
Arninator <sechzehngeteiltdurchneun@gmx.de>
2022-09-24 21:35:42 +0200

    create basic react compos of vita and aktuelles

---
+++ assets/aktuellesScript.js
@@ -0,0 +1,11 @@
+class Aktuelles extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+ReactDOM.render(<Aktuelles />, document.getElementById("aktuelles-page"));
---
+++ assets/vitaScript.js
@@ -0,0 +1,11 @@
+class Vita extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div></div>
+        )
+    }
+}
+ReactDOM.render(<Vita />, document.getElementById("vita-page"));
